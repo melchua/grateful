a787a9b203f8b072f8da4f42027b7d83eb51120c
Mel Chua <mchuam@gmail.com>
2021-03-08 08:14:42 -0800

    Prettier

--- grateful-webapp/services/gratitudes.js
+++ grateful-webapp/services/gratitudes.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 // eslint-disable-next-line
 export const postGratitude = (user_id, description) => {
   try {
     axios
-      .post("http://localhost:7865/users/gratitudes/", { user_id, description })
+      .post('http://localhost:7865/users/gratitudes/', { user_id, description })
       .then(() => {});
   } catch (err) {
     // eslint-disable-next-line
