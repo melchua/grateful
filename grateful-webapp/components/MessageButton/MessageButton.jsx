272bd75509cc111d506199bf0d4440e807c132d2
Mel <mchuam@gmail.com>
2021-03-12 20:18:23 -0800

    Add lint staged (#29)
    * Refactor message button

---
+++ grateful-webapp/components/MessageButton/MessageButton.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+import sendGratitude from '../../services/twilio.ts';
+
+export default function MessageButton() {
+  // eslint-disable-next-line
+  const [grat, setGrat] = useState("I am grateful for sunny days");
+  // eslint-disable-next-line
+  const [phone, setPhone] = useState("+17789512508");
+
+  const sendText = () => {
+    sendGratitude(grat, phone);
+  };
+
+  return (
+    <button type="button" onClick={sendText}>
+      Send Text
+    </button>
+  );
+}
