c96d8b254e645edfec4b64140817c44115487498
Mel Chua <mchuam@gmail.com>
2021-03-21 21:14:18 -0700

    Add phone and is_verified to schema

--- server/sql/users.sql
+++ server/sql/users.sql
@@ -1,8 +1,10 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   sub VARCHAR(70) NOT NULL UNIQUE,
+  phone VARCHAR(20),
+  is_verified BOOLEAN DEFAULT FALSE,
   created_at timestamptz NOT NULL DEFAULT now()
 );
 
