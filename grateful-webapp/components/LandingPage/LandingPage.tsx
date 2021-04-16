4ca58a661735e0f12ed61587e5a1bd4df9f39fb1
Joseph Williams <josephwilliamsjs@gmail.com>
2021-04-16 16:17:24 -0400

    creating about page

--- grateful-webapp/components/LandingPage/LandingPage.tsx
+++ grateful-webapp/components/LandingPage/LandingPage.tsx
@@ -1,9 +1,12 @@
 import Button from '../Button/Button.tsx';
+import styles from '../../styles/LandingPage.module.css';
 
 export default function LandingPage() {
   return (
-    <div>
-      I am the landing page
+    <div className={styles.landingContainer}>
+      <h1>&#x260E; GRATEFUL</h1>
+      <h2>Your personalized gratitude journal.</h2>
+      <h4>with reminders messaged to you whenever you&apos;d like.</h4>
       <Button>
         <a href="/api/auth/login">GET STARTED</a>
       </Button>
